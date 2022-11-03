2939253e9f8ddc50be9d5058c65836b9c937f8bb
iam-migz <mareyes2132@gmail.com>
2022-11-03 17:06:50 +0800

    gracefull shutdown

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,14 +5,36 @@ const port = process.env.PORT || 4000;
 
 const MONGO_URI = process.env.MONGO_URI!;
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => {
-    console.log('connected to db');
-    app.listen(port, () => {
-      /* eslint-disable no-console */
-      console.log(`Listening: http://localhost:${port}`);
-      /* eslint-enable no-console */
+mongoose.connect(MONGO_URI);
+
+const server = app.listen(port, () => {
+  console.log(`Listening: http://localhost:${port} \nPID: ${process.pid}`);
+});
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('\nserver closed');
+    mongoose.connection.close(false, () => {
+      process.exit(0);
     });
-  })
-  .catch((err) => console.log(err));
+  });
+});
+
+process.on('SIGTERM', () => {
+  server.close(() => {
+    console.log('\nserver closed');
+    mongoose.connection.close(false, () => {
+      process.exit(0);
+    });
+  });
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('db connected');
+});
+mongoose.connection.on('disconnected', () => {
+  console.log('db disconnected ');
+});
+mongoose.connection.on('error', (err) => {
+  console.log('db error ', err.message);
+});
