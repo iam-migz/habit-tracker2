900342e953d5685d74e1e0bd687bed2385427beb
iam-migz <mareyes2132@gmail.com>
2022-12-24 21:34:23 +0800

    record resource

---
+++ server/src/controllers/record.controller.ts
@@ -0,0 +1,68 @@
+import { NextFunction, Request, Response } from 'express';
+import { createRecordInput, destroyRecordInput, indexRecordInput, showRecordInput } from '../schema/record.schema';
+import * as service from '../service/record.service';
+
+export async function createHandler(
+	req: Request<{}, {}, createRecordInput['body'], createRecordInput['query']>,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		const habitId = req.query.habitId;
+
+		const duplicate = await service.show({ date: req.body.date });
+		if (duplicate) {
+			res.status(409);
+			throw new Error('Record already exists');
+		}
+
+		const record = await service.create({ ...req.body, habitId });
+		res.status(201).json(record);
+	} catch (e) {
+		next(e);
+	}
+}
+
+export async function indexHandler(
+	req: Request<{}, {}, {}, indexRecordInput['query']>,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		const habitId = req.query.habitId;
+		const records = await service.index({ habitId });
+		res.json(records);
+	} catch (e) {
+		next(e);
+	}
+}
+
+export async function showHandler(req: Request<showRecordInput['params']>, res: Response, next: NextFunction) {
+	try {
+		const record = await service.show({ _id: req.params.recordId });
+		res.json(record);
+	} catch (e) {
+		next(e);
+	}
+}
+
+export async function destroyHandler(
+	req: Request<destroyRecordInput['params'], {}, {}>,
+	res: Response,
+	next: NextFunction
+) {
+	try {
+		const recordId = req.params.recordId;
+
+		const record = await service.show({ _id: recordId });
+		if (!record) {
+			res.status(404);
+			throw new Error('Record not found');
+		}
+
+		await service.destroy({ _id: record._id });
+		return res.sendStatus(204);
+	} catch (e) {
+		next(e);
+	}
+}
---
+++ server/src/models/record.model.ts
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import { HabitDoc } from './habit.model';
+
+export interface RecordInput {
+	habitId: HabitDoc['_id'];
+	date: Date;
+	image?: string;
+	note?: string;
+}
+
+export interface RecordDoc extends RecordInput, mongoose.Document {
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+const RecordSchema = new mongoose.Schema(
+	{
+		habitId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'Habit',
+			required: true,
+		},
+		date: {
+			type: Date,
+			unique: true,
+			required: true,
+		},
+		image: {
+			type: String,
+		},
+		note: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
+
+const RecordModel = mongoose.model<RecordDoc>('Record', RecordSchema);
+export default RecordModel;
