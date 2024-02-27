fc168eb24a199352ae7003cf6d16bc4aec11b330
iam-migz <mareyes2132@gmail.com>
2024-02-27 20:45:30 +0800

    docker-compose setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://stackoverflow.com/questions/68595151/unable-to-dockerize-vite-react-typescript-project/68595302#68595302
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
   plugins: [react()],
 });
